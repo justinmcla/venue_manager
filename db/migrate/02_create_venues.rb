604b9241990285474d48c250054d86699b445ce7
justinmcla <jwmclawhorn@gmail.com>
2020-08-06 23:37:30 -0400

    create venues table

---
+++ db/migrate/02_create_venues.rb
@@ -0,0 +1,14 @@
+class CreateVenues < ActiveRecord::Migrations[5.1]
+    def change
+        create_table :venues do |t|
+            t.string :name
+            t.string :address1
+            t.string :address2
+            t.string :city
+            t.string :state
+            t.integer :zip
+            t.string :stage
+            t.integer :seats
+        end
+    end
+end
