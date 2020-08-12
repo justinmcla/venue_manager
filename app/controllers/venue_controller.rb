727ec8fb096ec63e1c622fd0000bba895664cfba
justinmcla <jwmclawhorn@gmail.com>
2020-08-12 12:02:04 -0400

    built new_venue to user association

--- app/controllers/venue_controller.rb
+++ app/controllers/venue_controller.rb
@@ -6,10 +6,8 @@ class VenueController < ApplicationController
 
     post '/venues/new' do
         new_venue = Venue.create(params)
+        new_venue.user_id = current_user(session).id
         redirect to "/venues/#{new_venue.id}"
     end
-
-    
-
 
 end
