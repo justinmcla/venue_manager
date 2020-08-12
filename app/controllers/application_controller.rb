7f3589302c3b88c33e16522ccd92359bf0e6aa9e
justinmcla <jwmclawhorn@gmail.com>
2020-08-12 11:48:57 -0400

    configured sinatra-flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,11 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
     register Sinatra::ActiveRecordExtension
+    register Sinatra::Flash
 
     set :database, {adapter: "sqlite3", database: "db/venue.sqlite3"}
+    enable :sessions
+    set :session_secret, "development"
 
     configure do
         set :views, 'app/views'
