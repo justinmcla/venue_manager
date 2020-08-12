cbb2a99de8e372b10d99bbfc7e1ad8aa64dd02e7
justinmcla <jwmclawhorn@gmail.com>
2020-08-12 15:37:58 -0400

    added edit functionality for venues

--- app/controllers/venue_controller.rb
+++ app/controllers/venue_controller.rb
@@ -7,12 +7,28 @@ class VenueController < ApplicationController
     post '/venues/new' do
         new_venue = Venue.create(params)
         current_user(session).venues << new_venue
-        redirect to "/venues/#{new_venue.id}"
+        redirect "/venues/#{new_venue.id}"
+    end
+
+    get '/venues' do
+        erb :'venues/all'
     end
 
     get '/venues/:id' do
         @venue = Venue.find(params[:id])
         erb :'venues/venue'
+    end
+
+    get '/venues/:id/edit' do
+        @venue = Venue.find(params[:id])
+        erb :'venues/edit'
+    end
+
+    patch '/venues/:id' do
+        @venue = Venue.find(params[:id])
+        params.each { |key, val| @venue.send("#{key}=", val) if @venue.respond_to?("#{key}=") }
+        @venue.save
+        redirect to "/venues/#{@venue.id}"
     end
 
 end
