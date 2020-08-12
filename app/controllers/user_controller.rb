b001489587910238afc39c96b198665758126617
justinmcla <jwmclawhorn@gmail.com>
2020-08-12 11:49:54 -0400

    configured bcrypt functionality

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,24 +1,26 @@
 class UserController < ApplicationController
 
+    include BCrypt
+
     get "/signup" do
         erb :'user/signup'
       end
     
     post "/signup" do
         if params[:username].empty?
-            flash[:message] = "You must enter a username."
+            flash[:error] = "You must enter a username."
             redirect '/signup'
         end
         if User.all.any? { |u| u.username == params[:username] }
-            flash[:message] = "Username already in use."
+            flash[:error] = "Username already in use."
             redirect '/signup'
         end
-        user = User.new(first_name: params[:first_name], last_name: params[:last_name], username: params[:username], password_digest: params[:password])
+        user = User.new(first_name: params[:first_name], last_name: params[:last_name], username: params[:username], password_digest: Password.create(params[:password]))
         if user.save
             session[:user_id] = user.id
             redirect '/home'
         else
-            flash[:message] = "You must fill out all fields."
+            flash[:error] = "You must fill out all fields."
             redirect '/signup'
         end
     end
@@ -29,6 +31,31 @@ class UserController < ApplicationController
 
     get '/login' do
         erb :'user/login'
+    end
+
+    post '/login' do
+        user = User.find_by(username: params[:username])
+        if user && Password.new(user.password_digest) == params[:password]
+            session[:user_id] = user.id 
+            redirect '/home'
+        else
+            flash[:error] = "Invalid username/password."
+            redirect '/login'
+        end
+    end
+
+    get '/logout' do
+        session.clear
+        redirect '/'
+    end
+
+    helpers do
+        def is_logged_in?(session)
+            !!session[:user_id]
+        end
+        def current_user(session)
+            User.find(session[:user_id])
+        end
     end
 
 end
