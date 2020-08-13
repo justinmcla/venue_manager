e98008c5ded33ce7239ac30a1c3f58b4e955df5c
justinmcla <jwmclawhorn@gmail.com>
2020-08-12 22:08:41 -0400

    basic booking paths

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -1,6 +1,22 @@
 class BookingController < ApplicationController
 
-    
+    get '/bookings/new' do
+        erb :'bookings/new'
+    end
+
+    post '/bookings/new' do
+        new_booking = Booking.create(params)
+        new_booking.venue_id = params[:venue_id]
+        redirect to "/bookings/#{new_booking.id}"
+    end
+
+    get '/bookings/:id' do
+        @booking = Booking.find(params[:id])
+        erb :'bookings/booking'
+    end
+
+
+
 
 
 end
