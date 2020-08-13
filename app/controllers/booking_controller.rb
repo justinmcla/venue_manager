fa14ea5431149d60940e548063bcaaf545c5faf3
justinmcla <jwmclawhorn@gmail.com>
2020-08-13 15:02:18 -0400

    added booking edit paths

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -7,9 +7,6 @@ class BookingController < ApplicationController
     post '/bookings/new' do
         new_booking = Booking.create(params)
         new_booking.venue_id = params[:venue_id]
-        if params[:tenant_id] == '0'
-            redirect to "/tenants/new"
-        end
         redirect to "/bookings/#{new_booking.id}"
     end
 
@@ -18,8 +15,26 @@ class BookingController < ApplicationController
         erb :'bookings/booking'
     end
 
+    get '/bookings/:id/edit' do
+        @booking = Booking.find(params[:id])
+        erb :'bookings/edit'
+    end
+
+    patch '/bookings/:id' do
+        @booking = Booking.find(params[:id])
+        params.each { |key, val| @booking.send("#{key}=", val) if @booking.respond_to?("#{key}=") }
+        @booking.save
+        redirect to "/bookings/#{@booking.id}"
+    end
+
     get '/bookings' do
         erb :'bookings/all'
+    end
+
+    get '/bookings/:id/delete' do
+        @booking = Booking.find(params[:id])
+        @booking.destroy
+        redirect to "/bookings"
     end
 
 
