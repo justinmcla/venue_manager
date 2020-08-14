f311ebeffa6f9aad4c7c58af2a4b02951aa1bfe7
justinmcla <jwmclawhorn@gmail.com>
2020-08-14 13:23:57 -0400

    fixed associations when making new tenant during new booking

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -5,9 +5,18 @@ class BookingController < ApplicationController
     end
 
     post '/bookings/new' do
-        new_booking = Booking.create(params)
-        new_booking.venue_id = params[:venue_id]
-        redirect to "/bookings/#{new_booking.id}"
+        new_booking = Booking.create(name: params[:name])
+        params.each { |key, val| new_booking.send("#{key}=", val) if new_booking.respond_to?("#{key}=") }
+        new_booking.save
+        if params[:tenant_id] == '0' && !params[:new_tenant].empty?
+            new_tenant = Tenant.create(org_name: params[:new_tenant])
+            current_user(session).tenants << new_tenant
+            new_booking.tenant_id = new_tenant.id
+            new_booking.save
+            redirect to "/tenants/#{new_tenant.id}/edit"
+        else
+            redirect to "/bookings/#{new_booking.id}"
+        end
     end
 
     get '/bookings/:id' do
@@ -32,9 +41,13 @@ class BookingController < ApplicationController
     end
 
     get '/bookings/:id/delete' do
-        @booking = Booking.find(params[:id])
-        @booking.destroy
-        redirect to "/bookings"
+        if is_logged_in?(session) && current_user(session).bookings.include?(Booking.find(params[:id]))
+            @booking = Booking.find(params[:id])
+            @booking.destroy
+            redirect to "/bookings"
+        else
+            redirect to "/home"
+        end
     end
 
 
