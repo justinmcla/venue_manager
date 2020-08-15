deb36682ea37479438a8ffc26a7a8e8b04f61a91
justinmcla <jwmclawhorn@gmail.com>
2020-08-14 22:49:30 -0400

    changed column name

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2020_08_13_032808) do
 
   create_table "bookings", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "event_type"
     t.date "date"
     t.string "time"
     t.text "description"
