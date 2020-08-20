d686c0efd19a520280bdf1e71c174f30fd1f4953
justinmcla <jwmclawhorn@gmail.com>
2020-08-20 12:30:56 -0400

    set database to yml

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,7 @@ require 'bundler/setup'
 
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+set :database_file, "./database.yml"
+
 require './app/controllers/application_controller'
 require_all 'app'
