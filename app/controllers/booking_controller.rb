05a656f8b1ad78d400aa727add6134712e61ecea
justinmcla <jwmclawhorn@gmail.com>
2020-08-27 22:16:39 -0400

    refactored booking

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -10,16 +10,12 @@ class BookingController < ApplicationController
 
     post '/bookings' do
         new_booking = Booking.create(name: params[:name])
-        params.each { |key, val| new_booking.send("#{key}=", val) if new_booking.respond_to?("#{key}=") }
-        if !params[:booking].nil?
-            params[:booking][:employees].each { |employee| new_booking.employees << Employee.find(employee) }
-        end
-        new_booking.save
+        params[:employee_list].each { |employee| new_booking.employees << Employee.find(employee) } if params.has_key? :employee_list
+        update_safe(new_booking)
         if params[:tenant_id] == '0' && !params[:new_tenant].empty?
-            new_tenant = Tenant.create(org_name: params[:new_tenant])
-            current_user(session).tenants << new_tenant
-            new_booking.update(tenant_id: new_tenant.id)
-            redirect to "/tenants/#{new_tenant.id}/edit"
+            current_user(session).tenants << Tenant.create(org_name: params[:new_tenant])
+            new_booking.update(tenant_id: Tenant.last.id)
+            redirect to "/tenants/#{Tenant.last.id}/edit"
         else
             redirect to "/bookings/#{new_booking.id}"
         end
@@ -34,11 +30,8 @@ class BookingController < ApplicationController
     end
 
     patch '/bookings/:id' do
-        params.each { |key, val| @booking.send("#{key}=", val) if @booking.respond_to?("#{key}=") }
-        if !params[:booking].nil?
-            params[:booking][:employees].each { |employee| @booking.employees << Employee.find(employee) unless @booking.employees.include?(Employee.find(employee)) }
-        end
-        @booking.save
+        params[:employee_list].each { |employee| @booking.employees << Employee.find(employee) unless @booking.employees.include?(Employee.find(employee)) } if params.has_key? :employee_list
+        update_safe(@booking)
         redirect to "/bookings/#{@booking.id}"
     end
 
