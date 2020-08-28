29d411e37657276d2ce7976529c57a306fec5991
justinmcla <jwmclawhorn@gmail.com>
2020-08-27 22:22:52 -0400

    refactored inv controller

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -1,10 +1,5 @@
 class InventoryController < ApplicationController
 
-    before '/venues/:venue_id/inventories*' do
-        @venue = Venue.find(params[:venue_id])
-        redirect to "/venues" unless current_user(session).venues.include?(@venue)
-    end
-
     before '/venues/:venue_id/inventories/:inventory_id*' do
         pass if params[:inventory_id] == 'new'
         @inventory = Inventory.find(params[:inventory_id])
@@ -16,8 +11,7 @@ class InventoryController < ApplicationController
     end
 
     post '/venues/:venue_id/inventories' do
-        params.delete(:venue_id)
-        @venue.inventories << Inventory.create(params)
+        Inventory.create(params)
         redirect to "/venues/#{@venue.id}/inventories/#{Inventory.last.id}"
     end
 
