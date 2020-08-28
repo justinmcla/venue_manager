29c5e1fa10697b2c6e29277b61721820fb70d3e5
justinmcla <jwmclawhorn@gmail.com>
2020-08-27 22:52:42 -0400

    refactored item contr

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,13 +1,8 @@
 class ItemController < ApplicationController
 
-    before '/venues/:venue_id/inventories/:inventory_id*' do
+    before '/venues/:venue_id/inventories/:inventory_id/items/:item_id*' do
         @venue = Venue.find(params[:venue_id])
         @inventory = Inventory.find(params[:inventory_id])
-        redirect to "/venues/#{@venue.id}" unless current_user(session).venues.include?(@venue)
-        redirect to "/venues/#{@venue.id}" unless @venue.inventories.include?(@inventory)
-    end
-
-    before '/venues/:venue_id/inventories/:inventory_id/items/:item_id*' do
         pass if params[:item_id] == 'new'
         @item = Item.find(params[:item_id])
         redirect to "/venues/#{@venue.id}/inventories/#{@inventory.id}" unless @inventory.items.include?(@item)
@@ -19,8 +14,8 @@ class ItemController < ApplicationController
 
     post '/venues/:venue_id/inventories/:inventory_id/items' do
         params.delete(:venue_id)
-        @inventory.items << Item.create(params)
-        redirect to "/venues/#{@venue.id}/inventories/#{@inventory.id}"
+        Inventory.find(params[:inventory_id]).items << Item.create(params)
+        redirect to "/venues/#{@venue.id}/inventories/#{Inventory.find(params[:inventory_id]).id}"
     end
 
     get '/venues/:venue_id/inventories/:inventory_id/items/:id/edit' do
